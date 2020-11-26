5b15ec22f53637007ffdc226e564d87221a059d1
unknown <you@example.com>
2020-11-26 22:03:30 +0400

    release 3lab-for-web

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+    //Подключение шапки
+    require_once("header.php");
+?>
+
+<?php
+    //Подключение тела сайта
+    require_once("content.php");
+?>
+
---
+++ masters.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>bstart</title>
+</head>
+
+<body>
+
+
+
+  <div class="container-fluid">
+    <header class="exe">
+        
+        <?php
+            //Подключение шапки
+            require_once("header.php");
+        ?>
+
+      <div class="call-to row no-gutters">
+        <div class="col-12">
+          <div class="exe">
+            <p class="exe">Мы одни из немногих, кто работает в столь узкой сфере столько лет. Начиная с одного
+              поста, мы стали крупнейшей фирмой в стране, которая занимается ремонтом и тюнингом выхлопной системы, имея
+              возможность обслуживать одновременно 8 автомобилей, а штат вырос с 3-х мастеров до 22 работников,
+              включающих 12 высококвалифицированных мастеров, 3 старших мастеров, 2 управляющих, ежедневно следящих за
+              качеством выполняемых работ и др. </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+
+
+</body>
+
+</html>
